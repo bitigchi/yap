416f80114fad73cead0aed3cfa44eeec69229128
Emir Sarı <bitigchi@me.com>
2020-07-25 00:10:27 +0300

    Use brackets for date display

--- Sources/yap/Program.swift
+++ Sources/yap/Program.swift
@@ -158,7 +158,7 @@ final class Program {
                 formatter.dateStyle = .short
                 let dueDate = formatter.string(from: todo.dueDate)
                 let item = "\(index + 1)" + " - " + "\(todo.name)"
-                let itemWDate = "\(index + 1)" + " -" + "\(dueDate)" + "- "
+                let itemWDate = "\(index + 1)" + " <" + "\(dueDate)" + "> "
                     + "\(todo.name)"
                 
                 if noDate {
